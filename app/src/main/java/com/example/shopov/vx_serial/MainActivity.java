9ace52ec0b56e7c42f5f874cdb4e8476e387c83b
Stoyan Shopov <stoyan.shopov@gmail.com>
2016-08-09 14:10:04 +0300

    staging commit

--- app/src/main/java/com/example/shopov/vx_serial/MainActivity.java
+++ app/src/main/java/com/example/shopov/vx_serial/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.shopov.vx_serial;
 import android.content.Context;
 import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbManager;
-import android.hardware.usb.UsbRequest;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
@@ -20,89 +19,52 @@ import com.hoho.android.usbserial.driver.UsbSerialPort;
 import com.hoho.android.usbserial.driver.UsbSerialProber;
 
 import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    static private UsbSerialPort port = null;
 
     private Handler readVxHandler = new Handler() {
         @Override
         public final void handleMessage(Message msg) {
             // Update user interface
-            ((EditText)findViewById(R.id.editTextVxLog)).append("handler invoked");
+
+            ((EditText)findViewById(R.id.editTextVxLog)).append(":");
+            ((EditText)findViewById(R.id.editTextVxLog)).append((String) msg.obj);
         }
     };
 
-    Runnable runnable = new Runnable() {
+    Runnable runnableReadVxThread = new Runnable() {
         public void run() {
             // Insert network call here!
 
-
-
-            String btext = new String("???");
             Log.e("shopov", "shopov communicating with the vx");
 
-
-            // Find all available drivers from attached devices.
-            UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
-
-
-            ProbeTable customTable = new ProbeTable();
-            customTable.addProduct(0x1ad4, 0xb000, CdcAcmSerialDriver.class);
-
-            UsbSerialProber prober = new UsbSerialProber(customTable);
-            List<UsbSerialDriver> drivers = prober.findAllDrivers(manager);
-
-            List<UsbSerialDriver> availableDrivers = prober.findAllDrivers(manager);
-            if (availableDrivers.isEmpty()) {
-                //((Button)findViewById(R.id.button)).setText("no devices");
-                return;
-            }
-
-// Open a connection to the first available driver.
-            UsbSerialDriver driver = availableDrivers.get(0);
-            UsbDeviceConnection connection = manager.openDevice(driver.getDevice());
-            if (connection == null) {
-                // You probably need to call UsbManager.requestPermission(driver.getDevice(), ..)
-                ((Button)findViewById(R.id.button)).setText("need permissions?");
-                return;
-            }
-
 // Read some data! Most have just one port (port 0).
-            UsbSerialPort port = driver.getPorts().get(0);
-            try {
-                int i = 0;
-                port.open(connection);
-                if (true) {
-                    //((Button) findViewById(R.id.button)).setText("trying to write");
-                    port.write(new String("shopov\n").getBytes(), 2000);
+            synchronized (port) {
+                try {
+                    int i = 0;
+
+                    do {
+                        byte[] response = new byte[128];
+
+                        Log.e("shopov", "shopov xxx");
+                        i = port.read(response, 2000);
+                        Log.e("shopov", "shopov yyy, read: " + i);
+
+                        Message msg = Message.obtain();
+                        msg.obj = new String(Arrays.copyOf(response, i), "UTF-8");
+                        readVxHandler.sendMessage(msg);
+                    }
+                    while (i > 0);
+
+                } catch (Exception e) {
+                    Log.e("shopov", "shopov exception");
+                    e.printStackTrace();
                 }
-
-                if (false) do {
-                    byte[] response = new byte[128];
-                    ((Button) findViewById(R.id.button)).setText("trying to read");
-
-                    Log.e("shopov", "shopov xxx");
-                    i = port.read(response, 2000);
-                    Log.e("shopov", "shopov yyy, read: " + i);
-                    if (i == 0)
-                        btext += new String("no data");
-                    else
-                        btext += new String(Arrays.copyOf(response, i), "UTF-8");
-                    xxx++;
-                }
-                while (i > 0);
-
-                //((Button)findViewById(R.id.button)).setText(btext);
-
-            } catch (IOException e) {
-                e.printStackTrace();
-                //((Button)findViewById(R.id.button)).setText("error accessing port");
             }
-
-            readVxHandler.sendEmptyMessage(0);
         }
     };
 
@@ -112,14 +74,62 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    private static int xxx = 0;
-
     public void handleConnect(View view) {
 
-        Thread mythread = new Thread(runnable);
+        String btext = new String("???");
+        Log.e("shopov", "shopov communicating with the vx");
+
+
+        // Find all available drivers from attached devices.
+        UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+
+
+        ProbeTable customTable = new ProbeTable();
+        customTable.addProduct(0x1ad4, 0xb000, CdcAcmSerialDriver.class);
+
+        UsbSerialProber prober = new UsbSerialProber(customTable);
+        List<UsbSerialDriver> drivers = prober.findAllDrivers(manager);
+
+        List<UsbSerialDriver> availableDrivers = prober.findAllDrivers(manager);
+        if (availableDrivers.isEmpty()) {
+            ((Button)findViewById(R.id.button)).setText("no devices");
+            return;
+        }
+
+// Open a connection to the first available driver.
+        UsbSerialDriver driver = availableDrivers.get(0);
+        UsbDeviceConnection connection = manager.openDevice(driver.getDevice());
+        if (connection == null) {
+            // You probably need to call UsbManager.requestPermission(driver.getDevice(), ..)
+            ((Button)findViewById(R.id.button)).setText("need permissions?");
+            return;
+        }
+
+// Read some data! Most have just one port (port 0).
+        port = driver.getPorts().get(0);
+        try {
+            int i = 0;
+            port.open(connection);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            ((Button)findViewById(R.id.button)).setText("error accessing port");
+            return;
+        }
+        ((Button)findViewById(R.id.button)).setText("connected");
+        Thread mythread = new Thread(runnableReadVxThread);
         mythread.start();
     }
 
     public void handleSend(View view) {
+        synchronized (port) {
+            try {
+                port.write(new String(((EditText) findViewById(R.id.editTextVxDataToSend)).getText().toString()).getBytes(), 2000);
+                ((EditText) findViewById(R.id.editTextVxDataToSend)).getText().clear();
+            } catch (Exception e) {
+                Log.e("shopov", "shopov exception");
+                e.printStackTrace();
+            }
+        }
     }
 }
